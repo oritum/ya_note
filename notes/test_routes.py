53be9ec078d94bd5647055186ec311b4269b6137
Oleg Ritum <oritum@gmail.com>
2024-10-23 12:17:12 +0100

    1

--- notes/test_routes.py
+++ notes/test_routes.py
@@ -1,11 +1,7 @@
-# news/tests/test_routes.py
 from http import HTTPStatus
-
-# Импортируем функцию для определения модели пользователя.
-from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse_lazy, reverse
-
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from notes.models import Note
 
 User = get_user_model()
@@ -15,15 +11,15 @@ class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.author = User.objects.create(username='Oleg Ritum')
-        cls.reader = User.objects.create(username='Ana Ritum')
-        cls.note = Note.objects.create(title='Заголовок', text='Текст', author=cls.author)
-    
-    def test_pages_availability(self):
+        cls.author = User.objects.create(username='oritum')
+        cls.reader = User.objects.create(username='aritum')
+        cls.note = Note.objects.create(
+            title='слуг', text='Текст', author=cls.author
+        )
+
+    def test_pages_aviability(self):
         urls = (
             ('notes:home', None),
-            ('notes:detail', (self.note.slug,)),
-            ('notes:list', None),
             ('users:login', None),
             ('users:logout', None),
             ('users:signup', None),
@@ -34,33 +30,24 @@ class TestRoutes(TestCase):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_availability_for_notes_edit_and_delete(self):
+    def test_aviability_for_note_detail_edit_delete(self):
         users_statuses = (
-            (self.author, HTTPStatus.OK),
-            (self.reader, HTTPStatus.NOT_FOUND),
+             (self.author, HTTPStatus.OK),
+             (self.reader, HTTPStatus.NOT_FOUND),
         )
         for user, status in users_statuses:
             self.client.force_login(user)
-            for name in ('notes:edit', 'notes:delete'):
+            for name in ('notes:detail', 'notes:edit', 'notes:delete'):
                 with self.subTest(user=user, name=name):
-                    url = reverse_lazy('notes:success')
+                    url = reverse(name, args=(self.note.slug,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
 
     def test_redirect_for_anonymous_client(self):
-        # Сохраняем адрес страницы логина:
         login_url = reverse('users:login')
-        # В цикле перебираем имена страниц, с которых ожидаем редирект:
-        for name in ('notes:edit', 'notes:delete'):
+        for name in ('notes:edit', 'notes:delete', 'notes:detail'):
             with self.subTest(name=name):
-                # Получаем адрес страницы редактирования или удаления комментария:
                 url = reverse(name, args=(self.note.slug,))
-                # Получаем ожидаемый адрес страницы логина, 
-                # на который будет перенаправлен пользователь.
-                # Учитываем, что в адресе будет параметр next, в котором передаётся
-                # адрес страницы, с которой пользователь был переадресован.
                 redirect_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
-                # Проверяем, что редирект приведёт именно на указанную ссылку.
                 self.assertRedirects(response, redirect_url)
-
